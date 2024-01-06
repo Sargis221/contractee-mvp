e27c992e5afa4667211b75504d0aa6261003eeff
Sargis <118741999+Sargis221@users.noreply.github.com>
2024-01-07 00:24:49 +0400

    Delete contractee directory

--- contractee/src/utils/functions/addDocumentToFirestore.js
+++
@@ -1,6 +0,0 @@
-import {database} from "./../../firebase";
-
-export const addDocumentToFirestore = async (fileData) => {
-    const docRef = await database.collection('uploadedDocuments').add(fileData);
-    return docRef.id; // Return document ID
-};
